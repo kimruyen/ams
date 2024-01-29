d2b29ee61388e3cc1ddb95f84dbf95206541da8c
kimruyen <kimruyen@naver.com>
2024-01-29 13:38:08 +0900

    Changes

--- uart/UI.py
+++ uart/UI.py
@@ -11,13 +11,15 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 # CPU -> MICOM response command
 state = [0x0A, 0x0B, 0x0C, 0x0D]
 
+global data
+
 
 class Ui_MainWindow(QMainWindow):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(610, 410)
-        MainWindow.setMinimumSize(QtCore.QSize(610, 410))
-        MainWindow.setMaximumSize(QtCore.QSize(610, 410))
+        MainWindow.resize(610, 510)
+        MainWindow.setMinimumSize(QtCore.QSize(610, 510))
+        MainWindow.setMaximumSize(QtCore.QSize(610, 510))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
@@ -114,11 +116,8 @@ class Ui_MainWindow(QMainWindow):
         self.delBtn.setObjectName("delBtn")
         self.horizontalLayout.addWidget(self.delBtn)
         self.verticalLayout.addLayout(self.horizontalLayout)
-        self.mainBox = QtWidgets.QListView(self.centralwidget)
-        self.mainBox.setGeometry(QtCore.QRect(200, 10, 401, 351))
-        self.mainBox.setObjectName("mainBox")
         self.startBtn = QtWidgets.QPushButton(self.centralwidget)
-        self.startBtn.setGeometry(QtCore.QRect(100, 330, 91, 31))
+        self.startBtn.setGeometry(QtCore.QRect(110, 430, 91, 31))
         font = QtGui.QFont()
         font.setFamily("Agency FB")
         font.setPointSize(11)
@@ -127,7 +126,7 @@ class Ui_MainWindow(QMainWindow):
         self.startBtn.setFont(font)
         self.startBtn.setObjectName("startBtn")
         self.filepath = QtWidgets.QPushButton(self.centralwidget)
-        self.filepath.setGeometry(QtCore.QRect(10, 330, 81, 31))
+        self.filepath.setGeometry(QtCore.QRect(10, 430, 81, 31))
         font = QtGui.QFont()
         font.setFamily("Agency FB")
         font.setPointSize(10)
@@ -136,7 +135,7 @@ class Ui_MainWindow(QMainWindow):
         self.filepath.setFont(font)
         self.filepath.setObjectName("filepath")
         self.clearbtn = QtWidgets.QPushButton(self.centralwidget)
-        self.clearbtn.setGeometry(QtCore.QRect(110, 220, 81, 31))
+        self.clearbtn.setGeometry(QtCore.QRect(110, 390, 91, 31))
         font = QtGui.QFont()
         font.setFamily("Arial")
         font.setPointSize(11)
@@ -144,6 +143,37 @@ class Ui_MainWindow(QMainWindow):
         font.setWeight(75)
         self.clearbtn.setFont(font)
         self.clearbtn.setObjectName("clearbtn")
+        self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(210, 10, 391, 451))
+        self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.textEdit_4 = QtWidgets.QTextEdit(self.verticalLayoutWidget_2)
+        self.textEdit_4.setEnabled(False)
+        self.textEdit_4.setMinimumSize(QtCore.QSize(85, 24))
+        self.textEdit_4.setMaximumSize(QtCore.QSize(85, 24))
+        self.textEdit_4.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.textEdit_4.setObjectName("textEdit_4")
+        self.horizontalLayout_3.addWidget(self.textEdit_4)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_3)
+        self.sendBox = QtWidgets.QListView(self.verticalLayoutWidget_2)
+        self.sendBox.setObjectName("sendBox")
+        self.verticalLayout_2.addWidget(self.sendBox)
+        self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        self.textEdit_5 = QtWidgets.QTextEdit(self.verticalLayoutWidget_2)
+        self.textEdit_5.setEnabled(False)
+        self.textEdit_5.setMinimumSize(QtCore.QSize(85, 0))
+        self.textEdit_5.setMaximumSize(QtCore.QSize(85, 24))
+        self.textEdit_5.setObjectName("textEdit_5")
+        self.horizontalLayout_4.addWidget(self.textEdit_5)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_4)
+        self.responseBox = QtWidgets.QListView(self.verticalLayoutWidget_2)
+        self.responseBox.setObjectName("responseBox")
+        self.verticalLayout_2.addWidget(self.responseBox)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 610, 21))
@@ -163,11 +193,17 @@ class Ui_MainWindow(QMainWindow):
                                                            "\n"
                                                            ""))
         self.textEdit.setHtml(_translate("MainWindow",
-                                         "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-                                         "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                         "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" "
+                                         "\"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                         "<html><head><meta name=\"qrichtext\" content=\"1\" /><style "
+                                         "type=\"text/css\">\n"
                                          "p, li { white-space: pre-wrap; }\n"
-                                         "</style></head><body style=\" font-family:\'Gulim\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-                                         "<p align=\"center\" style=\" margin-top:6px; margin-bottom:6px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">BAUDRATE</span></p></body></html>"))
+                                         "</style></head><body style=\" font-family:\'Gulim\'; font-size:9pt; "
+                                         "font-weight:400; font-style:normal;\">\n"
+                                         "<p align=\"center\" style=\" margin-top:6px; margin-bottom:6px; "
+                                         "margin-left:0px; margin-right:0px; -qt-block-indent:0; "
+                                         "text-indent:0px;\"><span style=\" "
+                                         "font-weight:600;\">BAUDRATE</span></p></body></html>"))
         self.baudrateBox.setCurrentText(_translate("MainWindow", "9600"))
         self.baudrateBox.setItemText(0, _translate("MainWindow", "9600"))
         self.baudrateBox.setItemText(1, _translate("MainWindow", "19200"))
@@ -175,26 +211,61 @@ class Ui_MainWindow(QMainWindow):
         self.baudrateBox.setItemText(3, _translate("MainWindow", "57600"))
         self.baudrateBox.setItemText(4, _translate("MainWindow", "115200"))
         self.textEdit_2.setHtml(_translate("MainWindow",
-                                           "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-                                           "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                           "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" "
+                                           "\"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                           "<html><head><meta name=\"qrichtext\" content=\"1\" /><style "
+                                           "type=\"text/css\">\n"
                                            "p, li { white-space: pre-wrap; }\n"
-                                           "</style></head><body style=\" font-family:\'Agency FB\'; font-size:9pt; font-weight:600; font-style:normal;\">\n"
-                                           "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Gulim\';\">STATE</span></p></body></html>"))
+                                           "</style></head><body style=\" font-family:\'Agency FB\'; font-size:9pt; "
+                                           "font-weight:600; font-style:normal;\">\n"
+                                           "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; "
+                                           "margin-left:0px; margin-right:0px; -qt-block-indent:0; "
+                                           "text-indent:0px;\"><span style=\" "
+                                           "font-family:\'Gulim\';\">STATE</span></p></body></html>"))
         self.stateBox.setItemText(0, _translate("MainWindow", "CPU->MICOM / SEND"))
         self.stateBox.setItemText(1, _translate("MainWindow", "CPU->MICOM / RESPONSE"))
         self.stateBox.setItemText(2, _translate("MainWindow", "MICOM->CPU / SEND"))
         self.stateBox.setItemText(3, _translate("MainWindow", "MICOM->CPU / RESPONSE"))
         self.textEdit_3.setHtml(_translate("MainWindow",
-                                           "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-                                           "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                           "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" "
+                                           "\"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                           "<html><head><meta name=\"qrichtext\" content=\"1\" /><style "
+                                           "type=\"text/css\">\n"
                                            "p, li { white-space: pre-wrap; }\n"
-                                           "</style></head><body style=\" font-family:\'Agency FB\'; font-size:9pt; font-weight:600; font-style:normal;\">\n"
-                                           "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Gulim\';\">COMMAND ID</span></p></body></html>"))
+                                           "</style></head><body style=\" font-family:\'Agency FB\'; font-size:9pt; "
+                                           "font-weight:600; font-style:normal;\">\n"
+                                           "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; "
+                                           "margin-left:0px; margin-right:0px; -qt-block-indent:0; "
+                                           "text-indent:0px;\"><span style=\" font-family:\'Gulim\';\">COMMAND "
+                                           "ID</span></p></body></html>"))
         self.addBtn.setText(_translate("MainWindow", "추가"))
         self.delBtn.setText(_translate("MainWindow", "삭제"))
         self.startBtn.setText(_translate("MainWindow", "실행"))
         self.filepath.setText(_translate("MainWindow", "파일 선택"))
         self.clearbtn.setText(_translate("MainWindow", "CLEAR"))
+        self.textEdit_4.setHtml(_translate("MainWindow",
+                                           "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" "
+                                           "\"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                           "<html><head><meta name=\"qrichtext\" content=\"1\" /><style"
+                                           "type=\"text/css\">\n"
+                                           "p, li { white-space: pre-wrap; }\n"
+                                           "</style></head><body style=\" font-family:\'Gulim\'; font-size:9pt; "
+                                           "font-weight:400; font-style:normal;\">\n"
+                                           "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px;"
+                                           "margin-left:0px; margin-right:0px; -qt-block-indent:0; "
+                                           "text-indent:0px;\"><span style=\" "
+                                           "font-weight:600;\">SEND</span></p></body></html>"))
+        self.textEdit_5.setHtml(_translate("MainWindow",
+                                           "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" "
+                                           "\"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                           "<html><head><meta name=\"qrichtext\" content=\"1\" /><style"
+                                           "type=\"text/css\">\n"
+                                           "p, li { white-space: pre-wrap; }\n"
+                                           "</style></head><body style=\" font-family:\'Gulim\'; font-size:9pt; "
+                                           "font-weight:400; font-style:normal;\">\n"
+                                           "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; "
+                                           "margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" "
+                                           "font-weight:600;\">RESPONSE</span></p></body></html>"))
 
         self.startBtn.clicked.connect(self.btnEvent)
         self.filepath.clicked.connect(self.pathEvent)
@@ -226,39 +297,42 @@ class Ui_MainWindow(QMainWindow):
 
             baudRate = self.baudrateBox.currentText()
 
-            ser = uart(port, baudRate)
             # data => start(1) / state(1) / length(2) / command id(1) / data0 ~ dataN
             header = ['0x02']
 
             direction = self.stateBox.currentText()
 
             if direction == 'CPU->MICOM / SEND':
-                header[1] += state[0]
+                if data is not None:
+                    header[1] += state[0]
 
-                length = hex(5 + len(data))
-                header.append(length)
+                    length = hex(5 + len(data))
+                    header.append(length)
 
-                commandId = self.comboBox.currentText()
-                header.append(str(commandId))
+                    commandId = self.comboBox.currentText()
+                    header.append(str(commandId))
 
-                # send data
-                result = header + data
+                    # send data
+                    result = header + data
 
-                # checksum
-                checksum = 0
-                for i in result:
-                    checksum += int(i, 0)
-                checksum = hex(checksum)[:2] + hex(checksum)[-2:]
+                    # checksum
+                    checksum = 0
+                    for i in result:
+                        checksum += int(i, 0)
+                    checksum = hex(checksum)[:2] + hex(checksum)[-2:]
 
-                result += checksum
-                # send
-                for i in result:
-                    ser.write(bytes.fromhex(i[2:]))
+                    result += checksum
+                    # send
+                    for i in result:
+                        with ser = uart(port, baudRate):
+                            ser.write(bytes.fromhex(i[2:]))
+                            self.sendBox.addItem(i[2:])
 
             elif direction == 'CPU->MICOM / RESPONSE':
                 header[1] += state[3]
             elif direction == 'MICOM->CPU / SEND':
                 header[1] += state[1]
+                ser.readlines()
             # 'MICOM->CPU / RESPONSE'
             else:
                 header[1] += state[2]
@@ -267,7 +341,8 @@ class Ui_MainWindow(QMainWindow):
             print(ex)
 
     def clearEvent(self):
-        self.mainBox.clear()
+        self.responseBox.clear()
+        self.sendBox.clear()
 
     def addEvent(self):
         def add_event():
